7b8fa3e6876ee84850f515d07ca7b71f5d81c960
koz <tik.kozawa@gmail.com>
2026-05-10 16:26:05 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+async function loadComponent(id, file){
+
+  const response =
+    await fetch(file);
+
+  const html =
+    await response.text();
+
+  document.getElementById(id).innerHTML =
+    html;
+}
+
+/* 読み込み */
+
+loadComponent(
+  "header",
+  "./header.html"
+);
+
+loadComponent(
+  "footer",
+  "./footer.html"
+);
---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+const canvas = document.getElementById("noise");
+const ctx = canvas.getContext("2d");
+
+function resize(){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+resize();
+window.addEventListener("resize", resize);
+
+function drawNoise(){
+
+  const imageData = ctx.createImageData(
+    canvas.width,
+    canvas.height
+  );
+
+  const buffer = imageData.data;
+
+  for(let i = 0; i < buffer.length; i += 4){
+
+    const value = Math.random() * 255;
+
+    // RGBを少しズラしてグリッチ感
+    buffer[i] = value + Math.random() * 40;
+    buffer[i + 1] = value;
+    buffer[i + 2] = value + Math.random() * 80;
+
+    buffer[i + 3] = 45;
+  }
+
+  ctx.putImageData(imageData, 0, 0);
+
+  requestAnimationFrame(drawNoise);
+}
+
+drawNoise();
